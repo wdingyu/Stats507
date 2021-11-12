eba3cb4f0b00a6bec987b03d85a498485aed13dc
wdingyu <wdingyu@umich.edu>
2021-11-12 18:53:50 -0500

    this is used for gitHub collaboration

---
+++ pandas_notes/pd_topic_group1_4j_wdingyu.py
@@ -0,0 +1,84 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_json: true
+#     notebook_metadata_filter: markdown
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.5
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# ## Topics in Pandas
+# **Stats 507, Fall 2021** 
+#   
+
+# ## Contents
+# Add a bullet for each topic and link to the level 2 title header using 
+# the exact title with spaces replaced by a dash. 
+#
+# + [Concatenate](#Concatenate) 
+# + [Topic 2 Title](#Topic-2-Title)
+
+# ## Concatenate 
+# *Dingyu Wang*
+#
+# wdingyu@umich.edu
+
+
+# ## Concatenate 
+# + Concatenate pandas objects along a particular axis with optional set logic
+# along the other axes.
+# + Combine two Series.
+
+import pandas as pd
+
+s1 = pd.Series(['a', 'b', 'c'])
+s2 = pd.Series(['d', 'e', 'f'])
+pd.concat([s1, s2])
+
+# ## Concatenate 
+# * Concatenate pandas objects along a particular axis with optional set logic
+# along the other axes.
+# * Combine two Series.
+# * Combine two DataFrame objects with identical columns.
+
+df1 = pd.DataFrame([['a', 1], ['b', 2], ['c', 3]],
+                   columns=['letter', 'number'])
+df2 = pd.DataFrame([['d', 4], ['e', 5], ['f', 6]],
+                   columns=['letter', 'number'])
+pd.concat([df1, df2])
+
+# ## Concatenate 
+# * Concatenate pandas objects along a particular axis with optional set logic
+# along the other axes.
+# * Combine two Series.
+# * Combine two DataFrame objects with identical columns.
+# * Combine DataFrame objects with overlapping columns and return only those
+# that are shared by passing inner to the join keyword argument
+# (default outer).
+
+df3 = pd.DataFrame([['a', 1, 'Mary'], ['b', 2, 'John'], ['c', 3, 'James']],
+                   columns=['letter', 'number', 'name'])
+pd.concat([df1, df3])
+pd.concat([df1, df3], join='inner')
+
+# ## Concatenate 
+# * Concatenate pandas objects along a particular axis with optional set logic
+# along the other axes.
+# * Combine two Series.
+# * Combine two DataFrame objects with identical columns.
+# * Combine DataFrame objects with overlapping columns and return only those
+# that are shared by passing in `join=inner`(default outer).
+# * Combine DataFrame objects horizontally along the x axis by passing in
+# `axis=1`(default 0).
+
+df4 = pd.DataFrame([['Tom', 24], ['Jerry', 18], ['James', 22]],
+                   columns=['name', 'age'])
+pd.concat([df1, df4])
+pd.concat([df1, df4], axis=1)
